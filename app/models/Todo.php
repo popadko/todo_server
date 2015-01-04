bdb98de53e81941f21564170ee9307a1a7d87593
Oleg Popadko <oleg.popadko@icloud.com>
2015-01-04 15:29:45 +0200

    Improve model

--- app/models/Todo.php
+++ app/models/Todo.php
@@ -2,5 +2,25 @@
 
 class Todo extends \Illuminate\Database\Eloquent\Model
 {
-    protected $guarded = array();
+    protected $fillable = [
+        'id',
+        'title',
+        'completed',
+        'created_at',
+        'updated_at',
+    ];
+
+    public $incrementing = false;
+
+    public $timestamps = false;
+
+    public function setCreatedAtAttribute($time)
+    {
+        $this->attributes['created_at'] = intval($time) / 1000;
+    }
+
+    public function setUpdatedAtAttribute($time)
+    {
+        $this->attributes['updated_at'] = intval($time) / 1000;
+    }
 }
